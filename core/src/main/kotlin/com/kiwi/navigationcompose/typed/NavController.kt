a332beab4a9b9295de3bee8f156c04e456134738
Jan Skrasek <jan.skrasek@kiwi.com>
2022-09-07 10:21:21 +0200

    implement generic popUpTo()

--- core/src/main/kotlin/com/kiwi/navigationcompose/typed/NavController.kt
+++ core/src/main/kotlin/com/kiwi/navigationcompose/typed/NavController.kt
@@ -2,15 +2,43 @@ package com.kiwi.navigationcompose.typed
 
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
+import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.Navigator
+import androidx.navigation.PopUpToBuilder
+import kotlinx.serialization.ExperimentalSerializationApi
 
 /**
  * Navigates to the passed Route.
  */
 public fun NavController.navigate(
-	destination: Route,
+	route: Route,
 	navOptions: NavOptions? = null,
 	navigatorExtras: Navigator.Extras? = null,
 ) {
-	navigate(destination.url, navOptions, navigatorExtras)
+	navigate(route.url, navOptions, navigatorExtras)
+}
+
+@ExperimentalSerializationApi
+public inline fun <reified T : Destination> NavOptions.Builder.setPopUpTo(
+	inclusive: Boolean,
+	saveState: Boolean = false,
+): NavOptions.Builder {
+	return setPopUpTo(createRoutePattern<T>(), inclusive, saveState)
+}
+
+/**
+ * Navigates to the passed Route.
+ */
+public fun NavController.navigate(
+	route: Route,
+	builder: NavOptionsBuilder.() -> Unit,
+) {
+	navigate(route.url, builder)
+}
+
+@ExperimentalSerializationApi
+public inline fun <reified T : Destination> NavOptionsBuilder.popUpTo(
+	noinline popUpToBuilder: PopUpToBuilder.() -> Unit = {},
+) {
+	popUpTo(createRoutePattern<T>(), popUpToBuilder)
 }
