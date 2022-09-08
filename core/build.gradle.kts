93f94be9f29571fe4367b2556cb80293a82b5a34
Jan Skrasek <jan.skrasek@kiwi.com>
2022-09-08 12:30:39 +0200

    autoformat *.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,61 +1,61 @@
 @file:Suppress("DSL_SCOPE_VIOLATION")
 
 plugins {
-    id("com.android.library")
-    alias(libs.plugins.kotlin.android)
+	id("com.android.library")
+	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.mavenPublish)
-    alias(libs.plugins.kotlinter)
-    alias(libs.plugins.kotlin.serialization)
+	alias(libs.plugins.kotlinter)
+	alias(libs.plugins.kotlin.serialization)
 }
 
 android {
-    namespace = "com.kiwi.navigationcompose.typed"
+	namespace = "com.kiwi.navigationcompose.typed"
 
-    compileSdk = libs.versions.compileSdk.get().toInt()
+	compileSdk = libs.versions.compileSdk.get().toInt()
 
-    defaultConfig {
-        minSdk = libs.versions.minSdk.get().toInt()
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+	defaultConfig {
+		minSdk = libs.versions.minSdk.get().toInt()
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
 
-    buildFeatures {
-        compose = true
-        buildConfig = false
-    }
+	buildFeatures {
+		compose = true
+		buildConfig = false
+	}
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
+	composeOptions {
+		kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+	}
 
-    kotlinOptions {
-        freeCompilerArgs = freeCompilerArgs.toMutableList().apply {
-            add("-Xexplicit-api=strict")
-        }.toList()
-    }
+	kotlinOptions {
+		freeCompilerArgs = freeCompilerArgs.toMutableList().apply {
+			add("-Xexplicit-api=strict")
+		}.toList()
+	}
 
-    lint {
-        abortOnError = true
-        warningsAsErrors = true
-    }
+	lint {
+		abortOnError = true
+		warningsAsErrors = true
+	}
 }
 
 kotlinter {
-    reporters = arrayOf("json")
-    experimentalRules = true
+	reporters = arrayOf("json")
+	experimentalRules = true
 	disabledRules = arrayOf("filename")
 }
 
 dependencies {
-    implementation(libs.kotlin.stdlib)
-    implementation(libs.kotlin.serialization)
-    implementation(libs.compose.navigation)
-    implementation(libs.okhttp)
+	implementation(libs.kotlin.stdlib)
+	implementation(libs.kotlin.serialization)
+	implementation(libs.compose.navigation)
+	implementation(libs.okhttp)
 
-    testImplementation(libs.junit)
-    testImplementation(libs.roboletric)
+	testImplementation(libs.junit)
+	testImplementation(libs.roboletric)
 }
