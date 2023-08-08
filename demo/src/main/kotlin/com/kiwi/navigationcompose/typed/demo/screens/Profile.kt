c32a5812e079bea805b5a7bfad6a1a387f8b57c4
Jan Skrasek <jan.skrasek@kiwi.com>
2023-08-08 19:15:01 +0200

    fix mutable state to utilize IntState

--- demo/src/main/kotlin/com/kiwi/navigationcompose/typed/demo/screens/Profile.kt
+++ demo/src/main/kotlin/com/kiwi/navigationcompose/typed/demo/screens/Profile.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -25,7 +26,7 @@ import kotlinx.serialization.ExperimentalSerializationApi
 @Composable
 internal fun Profile(navController: NavController) {
 	var name by rememberSaveable { mutableStateOf("") }
-	var times by rememberSaveable { mutableStateOf(0) }
+	var times by rememberSaveable { mutableIntStateOf(0) }
 
 	DialogResultEffect(
 		currentRoutePattern = createRoutePattern<ProfileDestinations.Home>(),
